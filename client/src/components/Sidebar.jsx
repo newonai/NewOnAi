351a91b136b9647f22883aff0001528ee59a474d
aptenox-technology <info@newonai.com>
2025-10-16 20:17:02 +0100

    made changes to sdiebar in mobile view showing transparent background

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -20,7 +20,7 @@ const Sidebar = ({ sidebar, setSidebar }) => {
     const {signOut, openUserProfile} = useClerk()
 
   return (
-    <div className={`w-60 bg-theme-sidebar-bg border-r border-theme flex flex-col justify-between items-center max-sm:absolute top-14 bottom-0 ${sidebar ? 'translate-x-0' : 'max-sm:-translate-x-full'} transition-all duration-300 ease-in-out`}>
+    <div className={`w-60 bg-theme-sidebar-bg border-r border-theme flex flex-col justify-between items-center max-sm:fixed top-14 bottom-0 left-0 ${sidebar ? 'translate-x-0' : 'max-sm:-translate-x-full'} transition-all duration-300 ease-in-out z-40`}>
       <div className='my-7 w-full'>
         <img src={user.imageUrl} alt="User avatar" className='w-13 rounded-full mx-auto'/>
         <h1 className='mt-1 text-center text-theme-primary'>{user.fullName}</h1>
